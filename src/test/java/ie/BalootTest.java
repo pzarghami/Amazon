e5694a5273707d433d50b04d779f88e38a763550
farzinasadikavan <farzinasadikavan@gmail.com>
2023-02-28 16:45:43 +0330

    add test folder

---
+++ src/test/java/ie/BalootTest.java
@@ -0,0 +1,14 @@
+package ie;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class BalootTest {
+    Baloot baloot;
+
+    public void assertResponse(String message){
+
+    }
+}
