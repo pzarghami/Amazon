f707e2ef30d53166a7bd12e830c7b519406b22c8
pamida <parmidazarghami@gmail.com>
2023-05-08 19:29:44 +0330

    Add Sorting option in commodities page.

--- Front/baloot/src/component/CommodityPreview.js
+++ Front/baloot/src/component/CommodityPreview.js
@@ -1,23 +1,60 @@
-import { Link } from 'react-router-dom';
+
 import './CommodityPreview.css';
+import axios from "axios";
+import { Link, useLocation ,useNavigate} from "react-router-dom";
+import { useEffect, useInsertionEffect, useState } from "react";
 
 export default function CommodityPreview(props) {
-    const { image, name, inStock, price } = props;
+    const { id, image, name, inStock, price } = props;
+    const userId = localStorage.getItem('userId');
+    const navigate = useNavigate();
 
+    const [error, setError] = useState('');
+    const handleAddToBuylist = async () => {
+        try {
+            setError('');
+            const data = { commodityId: id }
+            const response = await axios.post('/users/' + userId + '/watchlist/', data);
+        } catch (e) {
+            console.log(e);
+            if (e.response.data.message === "inStock") {
+                setError("Out of Stock")
+            }
+            if (e.response.data.message === "price") {
+                setError("Your invetory is not enough")
+            }
+            if (e.response.data.message === "login") {
+                setError("Please login first")
+            }
+            else {
+                setError(" login first")
+            }
+
+        }
+    }
+    const handleLinkToUser =  async () => {
+        navigate('/users/' + id);
+    }
     return (
         <div class=" ml-0 product">
             <span class="title">{name}</span>
             <span class="stock">{inStock + " left in stock"}</span>
-            <img src={image} />
+
+            <img onClick={ handleLinkToUser} src={image} />
             <div class="actions">
                 <span class="price">
                     {price + "$"}
                 </span>
                 <div class="add-to-cart">
-                    <button type="button" className="btn btn-outline-brown">Add to cart</button>
+                    <span onClick={handleAddToBuylist} type="button" className="btn ">Add to cart</span>
                 </div>
             </div>
 
+            {error &&
+                <div className="text-center text-danger">
+                    <span className="lead text-danger">{error}</span>
+                </div>
+            }
         </div>
     );
 }
--- Front/baloot/src/pages/js/Commodities.js
+++ Front/baloot/src/pages/js/Commodities.js
@@ -7,9 +7,18 @@ import CommodityPreview from '../../component/CommodityPreview';
 import '../css/commodities.css'
 import '../css/root.css'
 import '../css/footer.css'
-import userEvent from '@testing-library/user-event';
 
+const nameCompare = (a, b) => {
+    if (a.name > b.name) return 1;
+    else if (a.name === b.name) return 0;
+    else return -1;
+};
 
+const priceCompare = (a, b) => {
+    if (a.price > b.price) return 1;
+    else if (a.price === b.price) return 0;
+    else return -1;
+};
 export default function Commodities() {
     const [commodities, setCommodities] = useState(null);
 
@@ -17,6 +26,7 @@ export default function Commodities() {
 
     const [filterBy, setFilterBy] = useState();
     const [searchValue, setSearchValue] = useState("");
+    const [activeSorting, setSortingActive] = useState(0);
 
     const location = useLocation();
 
@@ -39,6 +49,23 @@ export default function Commodities() {
         fetchData();
 
     }, []);
+
+    useEffect(() => {
+        let compareFunction;
+        if (activeSorting) {
+
+            compareFunction = nameCompare;
+        } else {
+
+            compareFunction = priceCompare;
+        }
+        if (commodities !== null) {
+            commodities.sort(compareFunction);
+            setCommodities(commodities.slice());
+        }
+
+    }, [activeSorting]);
+
     return (
         <>
             <div class="page-container">
@@ -46,26 +73,33 @@ export default function Commodities() {
                     <span class="available-sort-container">Available commodities</span>
                     <img class="toggle-sort-container" src={logo} alt="toggle" />
                     <span class="sort-by-sort-container">sort by:</span>
-                    <span class="sorted-by-sort-container">name</span>
-                    <div class="category-sort-container">
-                        <span class="align-middle">price</span>
-                    </div>
+                    {activeSorting ?
+                        <>
+                            <button className="sort-name-selectedButton ">name</button>
+                            <button onClick={() => setSortingActive(0)} class="category-sort-container-notselectedButton align-middle">price</button>
+                        </>
+                        :
+                        <>
+                            <button onClick={() => setSortingActive(1)} className=' sort-name-notselectedButton '>name</button>
+                            <button class="category-sort-container-selectedButton align-middle">price</button>
+                        </>
+
+                    }
                 </div>
                 <div class="products-container">
                     <div className="row p-3">
-                        {/* {commodities && 
-                        commodities[0].name} */}
                         {commodities &&
                             Object.keys(commodities).map((item) => (
                                 <div className="col-3 mb-3" key={item.id}>
-                                    <Link to={"/commodities/" + commodities[item].id}>
-                                        <CommodityPreview
-                                            image={commodities[item].image}
-                                            name={commodities[item].name}
-                                            inStock={commodities[item].inStock}
-                                            price={commodities[item].price}
-                                        />
-                                    </Link>
+
+                                    <CommodityPreview
+                                        id={commodities[item].id}
+                                        image={commodities[item].image}
+                                        name={commodities[item].name}
+                                        inStock={commodities[item].inStock}
+                                        price={commodities[item].price}
+                                    />
+
                                 </div>
 
                             ))}
