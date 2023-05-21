5d717af4685576dfd5095fee91b375c0bb7c8e6f
pamida <parmidazarghami@gmail.com>
2023-05-21 17:01:15 +0330

    Add user.html to user.js

--- Front/baloot/src/Router.js
+++ Front/baloot/src/Router.js
@@ -6,6 +6,7 @@ import Commodities from "./pages/js/Commodities";
 import Layout from "./layout/Layout";
 import Commodity from "./pages/js/Commodity";
 import  Provider  from "./pages/js/Provider";
+import User from "./pages/js/User";
 
 
 export default function Router() {
@@ -19,6 +20,7 @@ export default function Router() {
         <Route path='/commodities' element={<Commodities />} />
         <Route path='/commodities/:id' element={<Commodity/>}/>
         <Route path='/provider/:id' element={<Provider/>}/>
+        <Route path='/user' element ={<User/>}/>
       </Route>
     </Routes>
   )
--- Front/baloot/src/pages/js/User.js
+++ Front/baloot/src/pages/js/User.js
@@ -1,5 +1,177 @@
+import '../css/user.css'
 export default function User() {
     return (
+        <div class="page-container">
+        <div class="info">
+            <div class="information">
+                <img src="../assets/images/person.png" alt="person"/>
+                <span>Marshal</span>
+                <br/>
+                <img src="../assets/images/mail.png" alt="mail"/>
+                <span>Marshal.Mathers@gmail.com</span>
+                <br/>
+                <img src="../assets/images/date.png" alt="mail"/>
+                <span>1972/10/17</span>
+                <br/>
+                <img src="../assets/images/location.png" alt="mail"/>
+                <span>20785 Schultes Avenue, warren,MI 48091</span>
+            </div>
+            <div class="credit">
+                <span>$10000000</span>
+                <div class="amount">
+                    <span>$Amount</span>
+                </div>
+                <div class="add-credit">
+                    <span> Add More Credit</span>
+                </div>
+            </div>
+        </div>
+        <div class="cart-box">
+            <img src="../assets/images/buylist.png" alt="buylist"/>
+            <span>Cart</span>
+        </div>
+        <div class="topic-box">
+            <div class="image">
+                <span>Image</span>
+            </div>
+            <div class="name">
+                <span>Name</span>
+            </div>
+            <div class="cate">
+                <span>Categories</span>
+            </div>
+            <div class="price">
+                <span>price</span>
+            </div>
+            <div class="provider">
+                <span>Provider ID</span>
+            </div>
+            <div class="rating">
+                <span>Rating</span>
+            </div>
+            <div class="stock">
+                <span>In Stock</span>
+            </div>
+            <div class="in-cart">
+                <span>In Cart</span>
+            </div>
+
+        </div>
+        <div class="buylist">
+            <img src="../assets/images/s21.png" alt="s21"/>
+            <div class="name">
+                <span>galaxy S21</span>
+            </div>
+            <div class="cate">
+                <span>Technology,Phone</span>
+            </div>
+            <div class="price">
+                <span>$21000000</span>
+            </div>
+            <div class="provider">
+                <span>1234</span>
+            </div>
+            <div class="rating">
+                <span>8.3</span>
+            </div>
+            <div class="in-stock">
+                <span>17</span>
+            </div>
+            <div class="in-cart">
+                <span> - </span>
+                <span> 1 </span>
+                <span> + </span>
+            </div>
+        </div>
+        <div class="pay">
+            <span>Pay now!</span>
+        </div>
+        <div class="history-box">
+            <img src="../assets/images/history.png" alt="history"/>
+            <span>History</span>
+        </div>
+        <div class="topic-box">
+            <div class="image">
+                <span>Image</span>
+            </div>
+            <div class="name">
+                <span>Name</span>
+            </div>
+            <div class="cate">
+                <span>Categories</span>
+            </div>
+            <div class="price">
+                <span>price</span>
+            </div>
+            <div class="provider">
+                <span>Provider ID</span>
+            </div>
+            <div class="rating">
+                <span>Rating</span>
+            </div>
+            <div class="stock">
+                <span>In Stock</span>
+            </div>
+            <div class="quantity">
+                <span>quantity</span>
+            </div>
+        </div>
+        <div class="history">
+            <img src="../assets/images/s21.png" alt="s21"/>
+            <div class="name">
+                <span>Mom's
+                </span>
+                <br/>
+                <span>spaghetti</span>
+            </div>
+            <div class="cate">
+                <span>food</span>
+            </div>
+            <div class="price">
+                <span>$60000</span>
+            </div>
+            <div class="provider">
+                <span>313</span>
+            </div>
+            <div class="rating">
+                <span>10</span>
+            </div>
+            <div class="in-stock">
+                <span>0</span>
+            </div>
+            <div class="quantity">
+                <span>3</span>
+            </div>
+        </div>
+        <div class="history2">
+            <img src="../assets/images/mic.png" alt="mic"/>
+            <div class="name">
+                <span>Dre's
+                </span>
+                <br/>
+                <span>Microphone</span>
+            </div>
+            <div class="cate">
+                <span>Technology</span>
+            </div>
+            <div class="price">
+                <span>$4200000</span>
+            </div>
+            <div class="provider">
+                <span>4321</span>
+            </div>
+            <div class="rating">
+                <span>8.5</span>
+            </div>
+            <div class="in-stock">
+                <span>22</span>
+            </div>
+            <div class="quantity">
+                <span>1</span>
+            </div>
+        </div>
+
+    </div>
         
     )
 }
