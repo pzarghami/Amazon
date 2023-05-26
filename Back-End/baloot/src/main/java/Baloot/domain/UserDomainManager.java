12c0c3fdf3c491c3a5466dc9ca7bd704fcb436d6
farzinasadikavan <farzinasadikavan@gmail.com>
2023-05-26 16:29:47 +0330

    not login when signup

--- Back-End/baloot/src/main/java/Baloot/domain/UserDomainManager.java
+++ Back-End/baloot/src/main/java/Baloot/domain/UserDomainManager.java
@@ -8,32 +8,34 @@ import Baloot.Exeption.CustomException;
 
 public class UserDomainManager {
     private static UserDomainManager instance;
-    public static UserDomainManager getInstance(){
+
+    public static UserDomainManager getInstance() {
         if (instance == null) {
             instance = new UserDomainManager();
         }
         return instance;
     }
 
-    public UserDTO getUserDTO(){
+    public UserDTO getUserDTO() {
         return UserRepo.loggedInUser.getDTO();
     }
+
     public void loginUser(String username, String Password) throws CustomException {
         var user = UserRepo.getInstance().getElementById(username);
-        if(!user.checkPassword(Password)) {
+        if (!user.checkPassword(Password)) {
             throw new CustomException("password is wrong");
         }
         UserRepo.getInstance().loginUser(user);
     }
 
-    public void logoutUser(){
+    public void logoutUser() {
         UserRepo.getInstance().logoutUser();
     }
 
     public void registerUser(String username, String password, String email, String birthdate, String address) throws CustomException {
         var user = new User(username, password, email, birthdate, address, 0);
         UserRepo.getInstance().addElement(user);
-        UserRepo.getInstance().loginUser(user);
+        // UserRepo.getInstance().loginUser(user);
     }
 
     public void addToBuyList(int commodityId) throws CustomException {
@@ -42,9 +44,11 @@ public class UserDomainManager {
     }
 
     public void removeFromBuyList(int commodityId) throws CustomException {
-        var commodity =CommodityRepo.getInstance().getElementById(String.valueOf(commodityId));
+        var commodity = CommodityRepo.getInstance().getElementById(String.valueOf(commodityId));
         UserRepo.loggedInUser.removeFromBuyList(commodity);
     }
 
-    public void addCredit(int amount){UserRepo.loggedInUser.addCredit(amount);}
+    public void addCredit(int amount) {
+        UserRepo.loggedInUser.addCredit(amount);
+    }
 }
